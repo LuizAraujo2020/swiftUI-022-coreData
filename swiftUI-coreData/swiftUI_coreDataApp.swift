faaef136cfdf3fd0275e081ced5179c6232bfb60
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-12-05 12:48:55 -0300

    Initial Commit

---
+++ swiftUI-coreData/swiftUI_coreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_coreDataApp.swift
+//  swiftUI-coreData
+//
+//  Created by Luiz Araujo on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_coreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
